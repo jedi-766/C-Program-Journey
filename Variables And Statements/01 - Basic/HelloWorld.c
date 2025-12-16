5e57dcaf80ccd152eb2b290dbf4e28982c9af211
jed ivan <208831954+jedi-766@users.noreply.github.com>
2025-12-16 16:39:08 +0800

    Add HelloWorld program with colorful output
    This program prints a colorful greeting message in multiple languages followed by an ASCII art representation of 'Hello World'.

---
+++ Variables And Statements/01 - Basic/HelloWorld.c
@@ -0,0 +1,18 @@
+//My Hello World
+#include <stdio.h>
+
+int main() {
+printf("\033[31m");
+printf("Hello!, Hallo!, привет!, Bonjour!, Witam!, Hola!, Hej!, Ciao!, Kumusta!\n");
+printf("\033[0m");
+
+    printf("\033[32m");
+    printf("██╗  ██╗███████╗██╗     ██╗     ██████╗ \n");
+    printf("██║  ██║██╔════╝██║     ██║    ██╔═══██╗\n");
+    printf("███████║█████╗  ██║     ██║    ██║   ██║\n");
+    printf("██╔══██║██╔══╝  ██║     ██║    ██║   ██║\n");
+    printf("██║  ██║███████╗███████╗███████╗╚██████╔╝\n");
+    printf("╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ \n");
+    printf("\033[0m");
+    return 0;
+}
